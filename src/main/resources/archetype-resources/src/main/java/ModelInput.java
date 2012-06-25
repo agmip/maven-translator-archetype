8dbdfe1901b3ba68dd10fa7383e54369ac093ded
Christopher Villalobos <christopher.villalobos@gmail.com>
2012-06-24 23:30:18 -0400

    Initial Commit

---
+++ src/main/resources/archetype-resources/src/main/java/ModelInput.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import org.agmip.core.types.AdvancedHashMap;
+import org.agmip.core.types.TranslatorInput;
+
+public class ModelInput implements TranslatorInput {
+    public AdvancedHashMap readFile(String fileName) {
+        AdvancedHashMap map = new AdvancedHashMap();
+        return map;
+    }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/ModelOutput.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import java.io.File;
+import java.io.IOException;
+import org.agmip.core.types.AdvancedHashMap;
+import org.agmip.core.types.TranslatorOutput;
+import org.agmip.util.JSONAdapter;
+
+public class ModelOutput implements TranslatorOutput {
+    public void writeFile(String filePath, AdvancedHashMap input) {
+        // Write your file out here.
+    }
+}
